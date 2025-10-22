51e64d157fe8ea8fce0e516148150b868b9b3a48
Paola Novick <paola.novick@davinci.edu.ar>
2025-10-22 12:13:13 -0300

    estilo separador

--- src/components/Separador.jsx
+++ src/components/Separador.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 const Separador = () => {
   return (
-    <div className="w-full py-12 sm:py-16 md:py-20 bg-gradient-to-b from-gray-50 to-white overflow-hidden">
-      <div className="container mx-auto px-4">
-        <div className="relative h-32 sm:h-40 md:h-48 lg:h-56">
-          <img
-            src="https://i.ibb.co/KxGmH7j2/piernas.png"
-            alt="Separador decorativo"
-            className="w-full h-full object-contain object-center opacity-90 hover:opacity-100 transition-opacity duration-300"
-          />
-        </div>
+    <div
+      className="w-full h-screen bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://i.ibb.co/KxGmH7j2/piernas.png')`,
+      }}
+    >
+      <div className="flex items-center justify-center h-full bg-black/10">
+        {/* Opcional: Puedes agregar texto o dejarlo vacío para solo mostrar la imagen */}
       </div>
     </div>
   );
