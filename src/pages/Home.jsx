61ba94bf3ef49fce7ff7e8b3929bff43b9176654
Paola Novick <paola.novick@davinci.edu.ar>
2025-10-23 14:20:08 -0300

    FRONT IMG

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = ({ products }) => {
       <div
         className="w-full h-screen bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url('https://br.louisvuitton.com/images/is/image//content/dam/lv/editorial-content/New-Homepage/2025/central/collections/lv-ski/W_LV_SKI_WW_HP_MainPush3_October_DI3.jpg?wid=1090')`,
+          backgroundImage: `url('https://i.ibb.co/qF77tC31/separador.png')`,
         }}
       >
         <div className="flex items-center justify-center h-full bg-black/30">
