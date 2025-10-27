0b76fc13707dd4822f18f570bee88ef42166fe9b
Paola Novick <paola.novick@davinci.edu.ar>
2025-10-27 18:22:21 -0300

    tamano logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,8 +44,14 @@ export default function Footer() {
     <div className="footer bg-[#f2d9a0] text-kbcream">
       <div className="footer-container max-w-7xl mx-auto px-4 py-12">
         {/* Logo y columnas */}
-        <div className="footer-header">
-          <img src="/logo-SF.png" alt="Logo" className="footer-logo" />
+        <div className="footer-header flex flex-col items-center">
+  <img
+    src="/logo-SF.png"
+    alt="Logo"
+    className="footer-logo w-24 h-auto mb-4" // 👈 ancho fijo, alto automático
+  />
+</div>
+
          
           <div className="footer-top grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center w-full">
             {/* Columna 1: Secciones */}
